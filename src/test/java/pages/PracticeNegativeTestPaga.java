f979c5f7767a21106365ec54e3ae0056fef0a024
ismail <ismailciftci407@gmail.com>
2022-10-20 16:54:33 +0300

    20.10.22

--- src/test/java/pages/PracticeNegativeTestPaga.java
+++ src/test/java/pages/PracticeNegativeTestPaga.java
@@ -1,2 +1,40 @@
-package pages;public class PracticeNegativeTestPaga {
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class PracticeNegativeTestPaga {
+
+    public PracticeNegativeTestPaga(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//a[@class='d-flex align-items-center dropdown-toggle nav-link']")
+    public WebElement signinSymbol;
+
+    @FindBy(xpath = "//*[.='Sign in']")
+    public WebElement signinButton;
+
+    @FindBy(xpath = "//input[@id='username']")
+    public WebElement username;
+
+    @FindBy (xpath = "//input[@id='password']")
+    public WebElement password;
+
+    @FindBy (xpath = "(//*[.='Sign in'])[4]")
+    public WebElement signinWithCredentials;
+
+    @FindBy(xpath = "//*[contains(text(), 'Authentication information not correct.')]")
+    public WebElement popUpMessage;//ACILIP KAPANAN MESAJLAR ICIN kalip loked sadece tirnak icindeki mesaj degistirilerek
+                                   //tekrar kullanilabilir
+
+    //ACILIP KAPANAN MESAJLAR ICIN
+    //contains text() olayı tum web elementleri icinde,
+    // icerisinde text olarak 'şu'  text i iceren locateleri bul diyor.
+    // bunu baska bir text icerigi ile aranan metni değistirerek te kullanabiliriz
+
+
 }
--- src/test/java/stepDefinitions/PracticeNegativeTestStepDefs.java
+++ src/test/java/stepDefinitions/PracticeNegativeTestStepDefs.java
@@ -1,2 +1,56 @@
-package stepDefinitions;public class PracticeNegativeTestStepDefs {
+package stepDefinitions;
+
+import com.github.javafaker.Faker;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.PracticeNegativeTestPaga;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class PracticeNegativeTestStepDefs {
+    PracticeNegativeTestPaga pntp = new PracticeNegativeTestPaga();
+
+    @Given("kullanici {string} sayfasina gider.")
+    public void kullanici_sayfasina_gider(String Url) {
+       Driver.getDriver().get(Url);
+    }
+
+    @When("login olmak icin signin e tiklar")
+    public void login_olmak_icin_signin_e_tiklar() {
+        ReusableMethods.waitForClickablility(pntp.signinSymbol,5);
+        pntp.signinSymbol.click();
+        pntp.signinButton.click();
+
+
+    }
+    @When("username icin {string} kullanici adini gonderir")
+    public void username_icin_kullanici_adini_gonderir(String username) {
+        username= Faker.instance().name().username();
+       pntp.username.sendKeys(username);
+
+    }
+    @When("password icin {string} parolasini gonderir")
+    public void password_icin_parolasini_gonderir(String password) {
+        password=Faker.instance().internet().password();
+        pntp.password.sendKeys(password);
+
+    }
+    @When("signine tiklar")
+    public void signine_tiklar() {
+        pntp.signinWithCredentials.click();
+
+    }
+    @Then("Authentication information not correct uyarisini dogrular")
+    public void authentication_information_not_correct_uyarisini_dogrular() {
+     ReusableMethods.waitFor(5);
+       // WebDriverWait wait = new WebDriverWait(Driver.getDriver(),2);
+       // wait.until(ExpectedConditions.visibilityOf(pntp.popUpMessage));
+
+        Assert.assertTrue(pntp.popUpMessage.isDisplayed());
+
+    }
 }
